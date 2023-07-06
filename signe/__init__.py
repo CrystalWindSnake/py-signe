257a5af607091102ed22d4d9c8fe6f6e8578dd99
CrystalWindSnake <568166495@qq.com>
2023-07-06 13:26:16 +0800

    chore

--- signe/__init__.py
+++ signe/__init__.py
@@ -2,4 +2,4 @@ from signe.utils import effect, createSignal, computed, batch, cleanup, on
 from signe.reactive.proxy import createReactive, isReactive
 
 
-__version__ = "0.1.2"
+__version__ = "0.1.3"
