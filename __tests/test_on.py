8015143d8f0a2cf70ace0e5d299d2b02cde58c42
CrystalWindSnake <568166495@qq.com>
2024-01-05 03:07:15 +0800

    feat

--- __tests/test_on.py
+++ __tests/test_on.py
@@ -1,10 +1,7 @@
-import json
-import _imports
-import pytest
-from signe import createSignal, effect, on
+from typing import List
+from signe import createSignal, on
 import utils
-from typing import Callable
-import math
+from typing_extensions import Literal
 
 
 class Test_on:
@@ -63,3 +60,60 @@ class Test_on:
         assert fn_spy.calledTimes == 1
         assert dummy1 == 100
         assert dummy2 == 99
+
+    def test_watch_state(self):
+        class RunState:
+            def __init__(self, time: int) -> None:
+                self._time = time
+
+            def next_state(self):
+                self._time += 1
+
+            @property
+            def time(self):
+                return self._time
+
+        num1, set_num1 = createSignal(1)
+        num2, set_num2 = createSignal(2)
+
+        run_state_on1 = RunState(1)
+
+        @on(num1, onchanges=True)
+        def _on1(s1):
+            if run_state_on1.time == 1:
+                assert s1.previous == 1
+                assert s1.current == 666
+
+            if run_state_on1.time == 2:
+                assert s1.previous == 666
+                assert s1.current == 999
+
+            run_state_on1.next_state()
+
+        run_state_on2 = RunState(0)
+
+        @on([num1, num2])
+        def _on2(s1, s2):
+            if run_state_on2.time == 0:
+                assert s1.previous is None
+                assert s1.current == 1
+                assert s2.previous is None
+                assert s2.current == 2
+
+            if run_state_on2.time == 1:
+                assert s1.previous == 1
+                assert s1.current == 666
+                assert s2.previous is None
+                assert s2.current == 2
+
+            if run_state_on2.time == 2:
+                assert s1.previous == 1
+                assert s1.current == 666
+                assert s2.previous == 2
+                assert s2.current == 666
+
+            run_state_on2.next_state()
+
+        set_num1(666)
+
+        set_num2(666)
