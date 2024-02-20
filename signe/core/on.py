5a194553c1ed281682fbd9e945f4316df0abcfd5
CrystalWindSnake <568166495@qq.com>
2024-02-20 20:15:49 +0800

    refactor

--- signe/core/on.py
+++ signe/core/on.py
@@ -3,7 +3,7 @@ import inspect
 from signe.core import Effect
 from signe.core.context import get_executor
 from signe.core.reactive import is_reactive, track_all
-from signe.core.scope import IScope
+from signe.core.scope import _GLOBAL_SCOPE_MANAGER, IScope
 from typing import (
     Any,
     Dict,
@@ -18,7 +18,7 @@ from typing import (
     Generic,
 )
 
-from .types import TGetter, TGetterSignal, TSignal
+from .types import TGetter, TSignal
 from .signal import is_signal
 
 T = TypeVar("T")
@@ -141,7 +141,7 @@ def on(
         else:
             fn(*states)
 
-    scope = scope
+    scope = scope or _GLOBAL_SCOPE_MANAGER._get_last_scope()
     executor = get_executor()
 
     def trigger_fn(effect: Effect):
@@ -157,3 +157,69 @@ def on(
     )
 
     return effect
+
+
+# def on(
+#     getter: Union[TGetter, Sequence[TGetter]],
+#     fn: Optional[Callable[..., None]] = None,
+#     *,
+#     onchanges=False,
+#     effect_kws: Optional[Dict[str, Any]] = None,
+#     deep=False,
+#     scope: Optional[IScope] = None,
+# ):
+#     call_kws = {"onchanges": onchanges, "effect_kws": effect_kws, "deep": deep}
+
+#     if fn is None:
+
+#         def wrap_cp(fn: Callable[[], T]):
+#             return on(getter, fn, **call_kws, scope=scope)  # type: ignore
+
+#         return wrap_cp
+
+#     getters: List[OnGetterModel] = []
+#     if isinstance(getter, Sequence):
+#         getters = [OnGetterModel(g, deep) for g in getter]  # type: ignore
+#     else:
+#         getters = [OnGetterModel(getter, deep)]  # type: ignore
+
+#     targets = getters
+
+#     def getter_calls():
+#         return [g.get_value_without_track() for g in getters]
+
+#     args_count = _get_func_args_count(fn)
+#     prev_values = getter_calls()
+
+#     def real_fn():
+#         nonlocal prev_values
+
+#         current_values = getter_calls()  # type: ignore
+
+#         states = (
+#             WatchedState(cur, prev)
+#             for cur, prev in zip(current_values, prev_values or current_values)
+#         )
+
+#         prev_values = current_values
+#         if args_count == 0:
+#             fn()
+#         else:
+#             fn(*states)
+
+#     scope = scope
+#     executor = get_executor()
+
+#     def trigger_fn(effect: Effect):
+#         executor.get_current_scheduler().mark_update(effect)
+
+#     effect = Effect(
+#         fn=real_fn,
+#         trigger_fn=trigger_fn,
+#         immediate=not onchanges,
+#         on=targets,  # type: ignore
+#         **(effect_kws or {}),
+#         scope=scope,
+#     )
+
+#     return effect
