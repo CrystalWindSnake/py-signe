1efeb7872486ce3ba8320bd4a44f92eb1e5c8e61
CrystalWindSnake <568166495@qq.com>
2024-08-27 00:11:49 +0800

    testing

--- __tests/test_scheduler.py
+++ __tests/test_scheduler.py
@@ -1,9 +1,9 @@
-from signe import computed, effect, signal
+from signe import computed, effect, signal, batch
 from signe.core.runtime import ExecutionScheduler
 
 
 class Test_custom:
-    def test_base_(self):
+    def test_base(self):
         class CusScheduler(ExecutionScheduler):
             pass
 
@@ -31,3 +31,23 @@ class Test_custom:
 
         items.value[1]["value"] = True
         assert dummy == [1, 2]
+
+    def test_with_reactive_proxy(self):
+        class CusScheduler(ExecutionScheduler):
+            pass
+
+        new_scheduler = CusScheduler()
+
+        dummy = {"count": 0}
+        data = signal([], scheduler=new_scheduler)
+
+        @effect(scheduler=new_scheduler)
+        def _():
+            dummy["count"] += 1
+            len(data.value)
+
+            for _ in data.value:
+                pass
+
+        data.value.insert(0, 1)
+        assert dummy["count"] == 2
